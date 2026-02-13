8560c3108192992c83b1ab49f96409a2370de18f
Samarth249 <samarthpore197@gmail.com>
2026-02-14 01:54:28 +0530

    Fixed translation issues in Hindi and Marathi

--- krishi-sadhan-app/src/utils/translations.js
+++ krishi-sadhan-app/src/utils/translations.js
@@ -8,18 +8,18 @@ export const translations = {
         dashboard: "Dashboard",
     },
     hi: {
-        welcome: "कृषि में आपका स्वागत है",
+        welcome: "कृषिधन में आपका स्वागत है",
         findEquipment: "उपकरण खोजें",
         listEquipment: "अपने उपकरण सूचीबद्ध करें",
-        login: "लॉग इन करें",
+        login: "लॉगिन करें",
         register: "पंजीकरण करें",
         dashboard: "डैशबोर्ड",
     },
     mr: {
-        welcome: "कृषीधन मध्ये आपले स्वागत आहे",
+        welcome: "कृषीधनमध्ये आपले स्वागत आहे",
         findEquipment: "साधने शोधा",
         listEquipment: "आपली साधने सूचीबद्ध करा",
-        login: "लॉग इन करा",
+        login: "लॉगिन करा",
         register: "नोंदणी करा",
         dashboard: "डॅशबोर्ड",
     }
